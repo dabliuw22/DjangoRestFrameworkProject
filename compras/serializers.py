ecedc1ee9c56d5bda2f97a4449e7ebeadda517b0
dabliuw22 <wleylop22@gmail.com>
2017-11-30 20:12:15 -0500

    Modificando serializers OrdenSerializer, ItemsOrdenSerializer

--- compras/serializers.py
+++ compras/serializers.py
@@ -24,31 +24,41 @@ class ProductoSerializer(serializers.ModelSerializer):
 class OrdenSerializer(serializers.ModelSerializer):
     total_orden = serializers.ReadOnlyField(source = 'get_total')
     fecha_hora = serializers.SerializerMethodField()
+    cliente = ClienteSerializer()
+    #productos = ProductoSerializer(many = True)
+    items = serializers.SerializerMethodField()
 
     def get_fecha_hora(self, obj):
         return obj.fecha.strftime("%A, %d. %B %Y %I:%M%p")
+
+    def get_items(self, obj):
+        items = ItemsOrden.objects.filter(orden = obj)
+        serializer = ItemsOrdenSerializer(data = items, many = True)
+        serializer.is_valid()
+        return serializer.data
 
     class Meta:
         model = Orden
         fields = (
             'id',
             'cliente',
-            'productos',
             'fecha',
             'fecha_hora',
             'direccion',
             'estado',
+            'items',
             'total_orden',
         )
 
 class ItemsOrdenSerializer(serializers.ModelSerializer):
+    producto = serializers.ReadOnlyField(source = 'producto.nombre')
+    precio = serializers.ReadOnlyField(source = 'producto.precio')
 
     class Meta:
         model = ItemsOrden
         fields = (
-            'id',
-            'orden',
             'producto',
+            'precio',
             'precio_venta',
             'cantidad',
             'valor',
