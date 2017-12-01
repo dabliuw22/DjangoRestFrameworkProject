93ef2cf5cef40b8ac7cdd33bb425d7447ee6f656
dabliuw22 <wleylop22@gmail.com>
2017-12-01 15:14:47 -0500

    Agregando Consumidor API

--- compras/urls.py
+++ compras/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 
-from .views import ProductoCreate, ClienteCreate, OrdenCreate, ClienteViewSet
+from .views import ProductoCreate, ClienteCreate, OrdenCreate, ClienteViewSet, OrdenConsumerView, ProductoConsumerView, producto_consumer_add
 
 urlpatterns = [
     url(r'^producto/add$', ProductoCreate.as_view(), name = 'producto_create'),
     url(r'^cliente/add$', ClienteCreate.as_view(), name = 'cliente_create'),
     url(r'^orden/add$', OrdenCreate.as_view(), name = 'orden_create'),
     #url(r'^api/cliente/list$', ClienteViewSet.as_view(), name = 'cliente_api_list')
+    url(r'^orden/api/consumer$', OrdenConsumerView.as_view(), name = 'orden_consumer'),
+    url(r'^producto/api/consumer$', ProductoConsumerView.as_view(), name = 'producto_consumer'),
+    url(r'^producto/api/add$', producto_consumer_add, name = "api_add_producto"),
 ]
--- compras/views.py
+++ compras/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import CreateView
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, TemplateView
 from django.core.urlresolvers import reverse_lazy
 
 from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework import viewsets, filters
 from rest_framework.viewsets import ModelViewSet
+
+import requests
 
 from .models import Producto, Cliente, Orden, ItemsOrden
 from .forms import ProductoForm, ClienteForm, OrdenForm
@@ -55,10 +57,56 @@ class ProductoViewSet(ModelViewSet):
     search_fields = ('nombre','precio')
     ordering_fields = ('nombre','precio')
 
+    def create(self, request, *args, **kwargs):
+
+        return super().create(request, *args, **kwargs)
+
+
 class OrdenViewSet(ModelViewSet):
     queryset = Orden.objects.all()
     serializer_class = OrdenSerializer
 
+    def create(self, request, *args, **kwargs):
+        return super(OrdenViewSet, self).create(request, *args, **kwargs)
+
 class ItemOrdenViewSet(ModelViewSet):
     queryset = ItemsOrden.objects.all()
     serializer_class = ItemsOrdenSerializer
+
+# ------ Consumer API's --------
+
+class OrdenConsumerView(TemplateView):
+    template_name = 'compras/api_consumer.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(OrdenConsumerView, self).get_context_data(**kwargs)
+        response_json = requests.get('http://localhost:8000/api/orden/')
+        if response_json.status_code == 200:
+            context['json'] = response_json.json()
+            for o in response_json.json():
+                print(o)
+        return context
+
+class ProductoConsumerView(TemplateView):
+    template_name = 'compras/api_consumer.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductoConsumerView, self).get_context_data(**kwargs)
+        response_json = requests.get('http://localhost:8000/api/producto/')
+        if response_json.status_code == 200:
+            context['json'] = response_json.json()
+            for p in response_json.json():
+                print(p)
+        return context
+
+def producto_consumer_add(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            requests.post('http://localhost:8000/api/producto/',
+                          {'nombre': request.POST['nombre'], 'precio': request.POST['precio']})
+            return redirect('compras:producto_consumer')
+    else:
+        form = ProductoForm()
+    return render(request, 'compras/producto_create.html',
+                  {'form': form, 'productos': Producto.objects.all().order_by('nombre')})
